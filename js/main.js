357df08491af6e4b3d39197f3050ee091b67f960
AClibecap <libe0026@algonquinlive.com>
2019-04-11 12:31:37 -0400

    Add the html and js files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var newName = prompt('What is your name?');
+
+if (newName == 'Thomas') {
+  alert('Hey' + newName + ', our names are the same!');
+}
+else {
+  alert('Too bad our names are different, ' + newName + '.');
+}
