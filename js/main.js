2def28e8dcac9116a98bb18b0d3243bd1f08d65c
AClibecap <libe0026@algonquinlive.com>
2019-04-11 12:32:40 -0400

    Make changes to the indentation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var newName = prompt('What is your name?');
 
 if (newName == 'Thomas') {
   alert('Hey' + newName + ', our names are the same!');
-}
-else {
+} else {
   alert('Too bad our names are different, ' + newName + '.');
 }
